ee58eb26397bd4a8f82970c9e272e4732cc36ec3
Ankit Dware <sabindware@gmail.com>
2024-10-20 18:11:37 +0545

    passing message from client to server

--- core/consumers.py
+++ core/consumers.py
@@ -9,3 +9,9 @@ class ChatConsumer(WebsocketConsumer):
             "type" : "connection_established",
             "message" : "you are now connected!"
         }))
+
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        print("message: ", message)
